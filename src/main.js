85e54f0782756989d75f1cba7ddd2958a4fc1b5e
xredha <galihredha100@gmail.com>
2021-06-16 11:26:23 +0700

    final init

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue';
+
+import router from './router.js';
+import store from './store/index.js';
+import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+import BaseDialog from './components/ui/BaseDialog.vue';
+
+const BASE_URL = ''; // URL REALTIME DB YOUR FIREBASE
+const BASE_KEY = ''; // KEY PROJECT YOUR FIREBASE
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
+
+app.mount('#app');
+
+export { BASE_URL, BASE_KEY };
